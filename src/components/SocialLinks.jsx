7ed1accc0d905338d81fad6aecb85f5fdf0a4d16
Gokul kumar.K <140736710+Gokulkumar4000@users.noreply.github.com>
2025-05-10 14:42:10 +0530

    Update SocialLinks.jsx

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,71 +1,71 @@
-import React from 'react'
-import {FaGithub,FaLinkedin ,FaInstagram} from 'react-icons/fa';
-import {HiOutlineMail} from 'react-icons/hi';
-import {BsFillPersonLinesFill} from 'react-icons/bs';
-const SocialLinks = () => {
+import React from 'react';
+import { FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
 
-  const links=[
+const SocialLinks = () => {
+  const links = [
     {
-        id:1,
-        child:(
-            <>LinkedIn<FaLinkedin size={30}/></>
-        ),
-        href:'https://linkedin.com',
-        // style:'rounded-tr-md'
+      id: 1,
+      child: (
+        <>LinkedIn<FaLinkedin size={30} /></>
+      ),
+      href: 'https://linkedin.com',
     },
     {
-        id:2,
-        child:(
-            <>Github<FaGithub size={30}/></>
-        ),
-        href:'https://github.com/Gokulkumar4000',
-        // style:'rounded-tr-md'  
+      id: 2,
+      child: (
+        <>Github<FaGithub size={30} /></>
+      ),
+      href: 'https://github.com/Gokulkumar4000',
     },
     {
-        id:3,
-        child:(
-            <>Mail<HiOutlineMail size={30}/></>
-        ),
-        href:'mailto:gokulkumargk295@gmail.com',
-        // style:'rounded-tr-md'
+      id: 3,
+      child: (
+        <>Mail<HiOutlineMail size={30} /></>
+      ),
+      href: 'mailto:gokulkumargk295@gmail.com',
     },
     {
-        id:4,
-        child:(
-            <>Resume<BsFillPersonLinesFill size={30}/></>
-        ),
-        href:'https://drive.google.com/file/d/110t5f-IG75bLnrLCV3QCgcsTEyid7IOh/view?usp=drive_link',
-        style:'rounded-br-md',
-        download:true
+      id: 4,
+      child: (
+        <>Resume<BsFillPersonLinesFill size={30} /></>
+      ),
+      href: 'https://drive.google.com/file/d/110t5f-IG75bLnrLCV3QCgcsTEyid7IOh/view?usp=drive_link',
+      style: 'rounded-br-md',
+      download: 'GokulKumar_Resume'
     },
     {
-        id:5,
-        child:(
-            <>Instagram<FaInstagram size={30}/></>
-        ),
-        href:'https://instagram.com/g_o_k_.u_l',
-        // style:'rounded-tr-md'  
+      id: 5,
+      child: (
+        <>Instagram<FaInstagram size={30} /></>
+      ),
+      href: 'https://instagram.com/g_o_k_.u_l',
     },
   ];
 
-
   return (
     <div className='flex flex-col top-[35%] left-0 fixed'>
-        <ul>
-            {links.map(({id,child,href,style,download})=>(
-                <li key={id} className={' flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500'}>
-                <a href={href} className='flex justify-between items-center w-full text-white' download={download} target='_blank' rel='noreferrer'>
-                    <>
-                        {child}
-                    </>
-                </a>
-            </li>
-            ))}
-        </ul>
+      <ul>
+        {links.map(({ id, child, href, style, download }) => (
+          <li 
+            key={id} 
+            className={`flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 ${style || ''}`}
+          >
+            <a
+              href={href}
+              className='flex justify-between items-center w-full text-white'
+              download={download}
+              target='_blank'
+              rel='noreferrer noopener'
+            >
+              {child}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default SocialLinks
-
-in this code if i clickresume button it should download resume that is in assert folder in pdf format
+export default SocialLinks;
