da3c964a7212d37ed479f7c2abc092577204743f
Gokul kumar.K <140736710+Gokulkumar4000@users.noreply.github.com>
2025-05-10 14:36:32 +0530

    resume final

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import {FaGithub,FaLinkedin ,FaInstagram} from 'react-icons/fa';
 import {HiOutlineMail} from 'react-icons/hi';
 import {BsFillPersonLinesFill} from 'react-icons/bs';
-
 const SocialLinks = () => {
+
   const links=[
     {
         id:1,
@@ -36,7 +36,7 @@ const SocialLinks = () => {
         ),
         href:'https://drive.google.com/file/d/110t5f-IG75bLnrLCV3QCgcsTEyid7IOh/view?usp=drive_link',
         style:'rounded-br-md',
-        download: true
+        download:true
     },
     {
         id:5,
@@ -49,19 +49,15 @@ const SocialLinks = () => {
   ];
 
 
-return (
+  return (
     <div className='flex flex-col top-[35%] left-0 fixed'>
         <ul>
             {links.map(({id,child,href,style,download})=>(
-                <li key={id} className={`flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 ${style || ''}`}>
-                <a 
-                  href={href} 
-                  className='flex justify-between items-center w-full text-white' 
-                  download={download}
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                    {child}
+                <li key={id} className={' flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500'}>
+                <a href={href} className='flex justify-between items-center w-full text-white' download={download} target='_blank' rel='noreferrer'>
+                    <>
+                        {child}
+                    </>
                 </a>
             </li>
             ))}
@@ -71,3 +67,5 @@ return (
 }
 
 export default SocialLinks
+
+in this code if i clickresume button it should download resume that is in assert folder in pdf format
